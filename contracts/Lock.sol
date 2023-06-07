b24d8a8b5c2dd9c298d1b9573a495adf623bd1ce
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-06-07 08:12:00 +0500

    Add files via upload

---
+++ contracts/Lock.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+// contract Lock {
+//     uint public unlockTime;
+//     address payable public owner;
+
+//     event Withdrawal(uint amount, uint when);
+
+//     constructor(uint _unlockTime) payable {
+//         require(
+//             block.timestamp < _unlockTime,
+//             "Unlock time should be in the future"
+//         );
+
+//         unlockTime = _unlockTime;
+//         owner = payable(msg.sender);
+//     }
+
+//     function withdraw() public {
+//         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+//         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+//         require(block.timestamp >= unlockTime, "You can't withdraw yet");
+//         require(msg.sender == owner, "You aren't the owner");
+
+//         emit Withdrawal(address(this).balance, block.timestamp);
+
+//         owner.transfer(address(this).balance);
+//     }
+// }
+// contract Lock {
+//     string public name = "Rabees Hussain";
+//     string public email = "ranaverse@gmail.com";
+//     string public password = "*********";
+// }
+
+contract Lock {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log(
+            "The greeting will be change from %s to %s",
+            greeting,
+            _greeting
+        );
+        greeting = _greeting;
+    }
+}
